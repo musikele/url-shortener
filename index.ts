0a126423908f8a20380530d6ca17bd38ab6afbd2
Michele Nasti <michele.nasti@gmail.com>
2024-02-13 10:18:06 +0100

    install fastify formbody

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import Fastify from 'fastify';
+import FastifyStatic from '@fastify/static';
+import path from 'node:path';
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.register(FastifyStatic, {
+  root: path.join(__dirname, 'public'),
+});
+
+fastify.post('/shorten', async function handler(request, reply) {
+  console.log(request.body);
+  return { hello: 'world' };
+});
+
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
